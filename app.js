d83853a0023fcf8ce7692979c595f75266dc9b33
Reinca9 <102973857+Reinca9@users.noreply.github.com>
2023-07-20 00:06:08 +0200

    back

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const registrationController = require("./controllers/inscription.controller");
-const connexionController = require("./controllers/connexion.controller");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.post("/register", registrationController.registerUser);
-app.post("/connexion", connexionController.loginUser);
+// Import your controllers
+const registrationController = require('./controllers/inscription.controller');
+const connexionController = require('./controllers/connexion.controller');
+
+// Routes for user registration and login
+app.post('/register', registrationController.registerUser);
+app.post('/connexion', connexionController.loginUser);
 
 app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  console.log('Server listening on port 3000');
 });
